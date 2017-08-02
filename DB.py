50e24b0445f259d975e5dd78dd34a8e760e4ed88
Shan-Chun Kuo <joy961406@gmail.com>
2017-08-02 03:03:06 +0100

    Create SQLite database and table and insert data from CSV file

---
+++ DB.py
@@ -0,0 +1,33 @@
+# Create a database
+
+import sqlite3
+import csv
+from datetime import datetime
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+class createDB():
+
+    def readCSV(self, filename):
+        conn = sqlite3.connect('CIUK.db')
+        print 'DB Creation Successful!'
+        cur = conn.cursor()
+        # cur.execute('''DROP TABLE PRODUCTS;''')
+        cur.execute('''CREATE TABLE PRODUCTS
+				       (ID INTEGER PRIMARY KEY     AUTOINCREMENT,
+				       TITLE           TEXT    NOT NULL,
+				       DESCRIPTION     TEXT    NOT NULL,
+				       PRICE           INTEGER     NOT NULL,
+				       CREATED_AT      TIMESTAMP,
+				       UPDATED_AT      TIMESTAMP);''')
+        print 'Table Creation Successful!'
+        with open(filename) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                cur.execute("INSERT INTO PRODUCTS VALUES (null, ?, ?, ?, ?, ?);", (unicode(row[0]), unicode(row[1]), unicode(row[2]), datetime.now(), datetime.now()))
+        print 'Successfully read data from CSV file!'
+        conn.commit()
+        conn.close()
+
+c = createDB().readCSV('products.csv')
